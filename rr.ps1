fe588f5bdb5e114286bbd36186946bd8895c26cc
unknown <rene@reifenrath.dev>
2024-10-25 18:21:04 +0200

    cleanup

--- rr.ps1
+++ rr.ps1
@@ -1,5 +1,4 @@
-function test-command-exists {
-    param ($command)
+function test-command-exists($command) {
     $oldPreference = $ErrorActionPreference
     $ErrorActionPreference = 'stop'
     try {
@@ -21,112 +20,106 @@ function today {
 function week {
     Get-Power-History -StartTime (Get-Date).Date.AddDays(1 - (Get-Date).Date.DayOfWeek.value__)
 }
-function Get-Power-History {
-    [CmdletBinding()]
-    param(
-        [Parameter(
-            Mandatory = $false,
-            ValueFromPipeline = $true,
-            ValueFromPipelineByPropertyName = $true
-        )]
-        [DateTime]  $StartTime = (Get-Date).Date,
- 
-        [int]       $MaxEvents = 9999
-    )
- 
-    BEGIN {}
- 
-    PROCESS {
-        try {
-            $Computer = $env:COMPUTERNAME.ToUpper()
-            $EventList = Get-WinEvent -ComputerName $Computer -FilterHashtable @{
-                Logname   = 'system'
-                Id        = '1074', '6008', '42', '4647', '4624', '27'
-                StartTime = $StartTime
-            } -MaxEvents $MaxEvents -ErrorAction Stop
+function Get-Power-History([DateTime]$StartTime = (Get-Date).Date) {
+    $EventList = Get-WinEvent -FilterHashtable @{
+        Logname   = 'system'
+        Id        = '1074', '6008', '42', '4647', '4624', '27', '107', '566'
+        StartTime = $StartTime
+    }
 
-            foreach ($Event in $EventList) {
-                switch ($Event.Id) {
-                    # Source: User32
-                    # User initiated a shutdown
-                    # Shutdown type is logged as string in param5/Properties.value[4]
-                    1074 {
-                        [PSCustomObject]@{
-                            TimeStamp = $Event.TimeCreated
-                            State     = '-'
-                            Action    = $Event.Properties.value[4]
-                        }
-                    }
-                    # Source: Kernel-Boot
-                    # Boot-Type: Cold Boot from Full Shutdown
-                    { $_ -eq 27 -And $Event.Properties.value[0] -eq 0 } {
-                        [PSCustomObject]@{
-                            TimeStamp = $Event.TimeCreated
-                            State     = '+'
-                            Action    = 'boot'
-                        }
-                    }
-                    # Source: Kernel-Boot
-                    # Boot-Type: Fast Startup / Hybrid Boot
-                    { $_ -eq 27 -And $Event.Properties.value[0] -eq 1 } {
-                        [PSCustomObject]@{
-                            TimeStamp = $Event.TimeCreated
-                            State     = '+'
-                            Action    = 'fast startup'
-                        }
-                    }
-                    # Source: Kernel-Boot
-                    # Boot-Type: Resume from Hibernation
-                    { $_ -eq 27 -And $Event.Properties.value[0] -eq 2 } {
-                        [PSCustomObject]@{
-                            TimeStamp = $Event.TimeCreated
-                            State     = '+'
-                            Action    = 'wake'
-                        }
-                    }
-                    # Source: Kernel-Power
-                    # The system is entering sleep
-                    42 {
-                        [PSCustomObject]@{
-                            TimeStamp = $Event.TimeCreated
-                            State     = '-'
-                            Action    = 'sleep'
-                        }
-                    }
-                    # Source: EventLog
-                    # The previous system shutdown was unexpected
-                    6008 {
-                        [PSCustomObject]@{
-                            TimeStamp = $Event.TimeCreated
-                            State     = '+'
-                            Action    = 'startup after unexpected shutdown'
-                        }
-                    }
-                    # A user successfully logged on to a computer
-                    4624 {
-                        [PSCustomObject]@{
-                            TimeStamp = $Event.TimeCreated
-                            State     = '+'
-                            Action    = 'logon'
-                        }
-                    }
-                    # A user initiated the logoff process
-                    4647 {
-                        [PSCustomObject]@{
-                            TimeStamp = $Event.TimeCreated
-                            State     = '-'
-                            Action    = 'logoff'
-                        }
-                    }
+    foreach ($Event in $EventList) {
+        switch ($Event.Id) {
+            # Source: User32
+            # User initiated a shutdown
+            # Shutdown type is logged as string in param5/Properties.value[4]
+            1074 {
+                [PSCustomObject]@{
+                    TimeStamp = $Event.TimeCreated
+                    State     = '-'
+                    Action    = $Event.Properties.value[4]
+                }
+            }
+            # Source: Kernel-Boot
+            # Boot-Type: Cold Boot from Full Shutdown
+            { $_ -eq 27 -And $Event.Properties.value[0] -eq 0 } {
+                [PSCustomObject]@{
+                    TimeStamp = $Event.TimeCreated
+                    State     = '+'
+                    Action    = 'boot'
+                }
+            }
+            # Source: Kernel-Boot
+            # Boot-Type: Fast Startup / Hybrid Boot
+            { $_ -eq 27 -And $Event.Properties.value[0] -eq 1 } {
+                [PSCustomObject]@{
+                    TimeStamp = $Event.TimeCreated
+                    State     = '+'
+                    Action    = 'fast startup'
+                }
+            }
+            # Source: Kernel-Boot
+            # Boot-Type: Resume from Hibernation
+            { $_ -eq 27 -And $Event.Properties.value[0] -eq 2 } {
+                [PSCustomObject]@{
+                    TimeStamp = $Event.TimeCreated
+                    State     = '+'
+                    Action    = 'wake'
+                }
+            }
+            # Source: Kernel-Power
+            # The system is entering sleep
+            42 {
+                [PSCustomObject]@{
+                    TimeStamp = $Event.TimeCreated
+                    State     = '-'
+                    Action    = 'sleep'
+                }
+            }
+            # Source: Kernel-Power
+            # The system has resumed from sleep
+            107 {
+                [PSCustomObject]@{
+                    TimeStamp = $Event.TimeCreated
+                    State     = '+'
+                    Action    = 'wake'
+                }
+            }
+            # Source: Kernel-Power
+            # Power off for an unknown reason
+            { $_ -eq 566 -and $Event.Properties.value[1] -eq 1} {
+                [PSCustomObject]@{
+                    TimeStamp = $Event.TimeCreated
+                    State     = '-'
+                    Action    = 'unplug'
+                }
+            }
+            # Source: EventLog
+            # The previous system shutdown was unexpected
+            # 6008 {
+            #    [PSCustomObject]@{
+            #        TimeStamp = $Event.TimeCreated
+            #        State     = '+'
+            #        Action    = 'startup after unexpected shutdown'
+            #    }
+            # }
+            # A user successfully logged on to a computer
+            4624 {
+                [PSCustomObject]@{
+                    TimeStamp = $Event.TimeCreated
+                    State     = '+'
+                    Action    = 'logon'
+                }
+            }
+            # A user initiated the logoff process
+            4647 {
+                [PSCustomObject]@{
+                    TimeStamp = $Event.TimeCreated
+                    State     = '-'
+                    Action    = 'logoff'
                 }
             }
         }
-        catch {
-            Write-Error $_.Exception.Message
-        }
     }
- 
-    END {}
 }
 # Finds duplicated files in the current directory and sub-directories using their hashes.
 # More info here: https://stackoverflow.com/a/58677703
@@ -144,21 +137,23 @@ function aliases {
     Compare-Object (Get-Alias) (PowerShell -NoProfile { Get-Alias }) -Property Name | sort Name
 }
 function functions { Get-ChildItem function:\ }
+function edit-profile { edit $PROFILE.AllUsersAllHosts }
+
+# -----------
+# START: Dotnet Dev Functions
+# -----------
 function trust-certs { dotnet dev-certs https --trust }
 function clean-solution { Get-ChildItem -inc bin, obj -rec | Remove-Item -rec -force }
-function update-visual-studio
-(
-    [string]$path
-) {
+function update-visual-studio([string]$path) {
     Start-Process -FilePath $path -ArgumentList "updateall --quiet --force"
 }
+# -----------
+# END: Dotnet Dev Functions
+# -----------
 
 # -----------
 # START: Aliases & Functions from https://github.com/ChrisTitusTech/powershell-profile
 # -----------
-function edit-profile {
-    edit $PROFILE.AllUsersAllHosts
-}
 Set-Alias ff find-file
 function find-file($name) {
     Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
